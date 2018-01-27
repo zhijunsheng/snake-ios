2b1e445f7e467465c80239983e0323cf23b4e4e1
BuildTools <unconfigured@null.spigotmc.org>
2018-01-26 20:14:04 -0500

    created struct Point and Board

---
+++ snake/model/Board.swift
@@ -0,0 +1,14 @@
+//
+//  Board.swift
+//  snake
+//
+//  Created by Nicole Han on 2018-01-26.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Board {
+    var numRows : Int
+    var numCols: Int
+}
---
+++ snake/model/Point.swift
@@ -0,0 +1,16 @@
+//
+//  Point.swift
+//  snake
+//
+//  Created by Nicole Han on 2018-01-26.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Point {
+    var row : Int
+    var col : Int
+}
+
+
