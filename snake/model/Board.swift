c1c4caf2bebe2f28a0e2b5f33944c18c46c381b9
BuildTools <unconfigured@null.spigotmc.org>
2018-01-26 21:03:34 -0500

    created Board Description

--- snake/model/Board.swift
+++ snake/model/Board.swift
@@ -8,7 +8,38 @@
 
 import Foundation
 
-struct Board {
+struct Board: CustomStringConvertible {
+    
     var numRows : Int
     var numCols: Int
+    
+    var description: String {
+        
+        let bottomChars = "+ A B C D E F G H J K L M N O P Q R S T"
+        
+        var dots = ""
+        
+        for _ in 1...numRows {
+
+            for _ in 1...numCols {
+                dots += " ."
+                
+            }
+            dots += "\n"
+
+        }
+        
+        let x = numCols * 2 + 1
+        
+        print("x is \(x)")
+        
+        let bottomCharsArray = Array(bottomChars)
+        let subString = bottomCharsArray[0...x]
+        print("subString is \(subString)")
+        
+        dots += subString
+        
+        return ("\(dots)")
+        
+    }
 }
--- snakeTests/PointTests.swift
+++ snakeTests/PointTests.swift
@@ -10,26 +10,14 @@ import XCTest
 
 class PointTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("-_-")
+        
+        let sum = add(a: 3, b: 5)
+        XCTAssertEqual(8, sum)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func add(a: Int, b: Int) -> Int {
+        return a + b
     }
     
 }
