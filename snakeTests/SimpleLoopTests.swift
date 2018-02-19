311ba432bc92c3720facedff1c90f7ecac64dd3a
BuildTools <unconfigured@null.spigotmc.org>
2018-02-18 19:33:43 -0500

    Created tests

--- snakeTests/SimpleLoopTests.swift
+++ snakeTests/SimpleLoopTests.swift
@@ -12,6 +12,66 @@ import XCTest
 
 class SimpleLoopTests: XCTestCase {
     
+    /*
+ Homework:
+     print:
+     1234567
+     234567
+     34567
+     4567
+     567
+     67
+     7
+     functions{
+     count multiples of 7 in [0...99]
+     
+     calculation of 1 + 4 + 9 + 16 + 25 ... n ^ 2
+     }
+ */
+    
+    func testPrint() {
+        var startingNumber = 1
+            for j in startingNumber...7 {
+                print(j, terminator: "")
+                if j == 7 {
+                    startingNumber += 1
+                }
+            }
+            print()
+    }
+    
+    
+    func testSumOfSquares() {
+        XCTAssertEqual(5, calculationOfSumOfSquares(lastInt: 2))
+        XCTAssertEqual(0, calculationOfSumOfSquares(lastInt: 0))
+
+    }
+    
+    func calculationOfSumOfSquares(lastInt: Int) -> Int {
+        var sum = 0
+        for i in 0...lastInt {
+            sum += i * i
+        }
+        return sum
+    }
+    
+    func testNumberOfMultiplesOf7() {
+        XCTAssertEqual(2, countMultiplesOf7InList(firstNumber: 3, lastNumber: 14))
+        XCTAssertEqual(0, countMultiplesOf7InList(firstNumber: 1, lastNumber: 1))
+        XCTAssertEqual(2, countMultiplesOf7InList(firstNumber: -1, lastNumber: 7))
+        XCTAssertEqual(3, countMultiplesOf7InList(firstNumber: -7, lastNumber: 7))
+    }
+    
+    func countMultiplesOf7InList(firstNumber: Int, lastNumber: Int) -> Int? {
+        var numberOfMultiplesOf7 = 0
+        for i in firstNumber...lastNumber {
+            if i % 7 == 0 {
+                numberOfMultiplesOf7 += 1
+            }
+        }
+        return numberOfMultiplesOf7
+    }
+  /*
     func testNumberOfOddNumbersInArr() {
         XCTAssertEqual(2, numberOfOddNumbersInArr(arr: [1, 5]))
         XCTAssertEqual(0, numberOfOddNumbersInArr(arr: [2]))
@@ -113,4 +173,5 @@ class SimpleLoopTests: XCTestCase {
         let sum = a + b
         return sum
     }
+ */
 }
