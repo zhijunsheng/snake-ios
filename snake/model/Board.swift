63fa277e75e4ea639174986a49a243d1cb99032c
Bowen Lin <bowenlin@lins-MacBook-Pro.local>
2018-06-26 21:04:47 -0400

    implemented the description of Board

--- snake/model/Board.swift
+++ snake/model/Board.swift
@@ -9,10 +9,30 @@
 import Foundation
 
 struct Board: CustomStringConvertible {
-    
     var description: String {
-        return "hi"
+        return generateBoardDesc(rows: 10, columns: 10)
     }
     
-    
+    func generateBoardDesc(rows: Int, columns: Int) -> String {
+        let numberOfDots = " ."
+        let letters = " + A B C D E F G H I J K L M N O P Q R W T U V W X Y Z"
+        var boardDesc = ""
+        
+        for i in 1...rows {
+            if i < 10 {
+                boardDesc += " \(i)"
+            } else {
+                boardDesc += "\(i)"
+            }
+            for a in 1...columns {
+                if a == columns {
+                    boardDesc += numberOfDots + "\n"
+                } else {
+                    boardDesc += numberOfDots
+                }
+            }
+        }
+        boardDesc += letters.prefix(2 + columns * 2)
+        return boardDesc
+    }
 }
---
+++ snakeTests/BoardTests.swift
@@ -0,0 +1,18 @@
+//
+//  BoardTests.swift
+//  snakeTests
+//
+//  Created by Bowen Lin on 2018-06-26.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import XCTest
+@testable import snake
+
+class BoardTests: XCTestCase {
+    
+    func testBoard() {
+        let board = Board()
+        print(board)
+    }
+}
