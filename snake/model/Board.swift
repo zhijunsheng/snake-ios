1ba4db0af4a4278f4951817a280443d7bd387969
Peter Shi <petershi@192.168.0.113>
2018-07-07 22:31:34 +0800

    turned snake into an empty app

--- snake/model/Board.swift
+++ snake/model/Board.swift
@@ -8,76 +8,7 @@
 
 import Foundation
 
-struct Board: CustomStringConvertible {
+struct Board {
     
-    var numRows : Int
-    var numCols: Int
-    var snake: [Point]
-    var food: [Point]
-    
-    func isOnBoard(point: Point) -> Bool {
-        return 1 <= point.row && point.row <= numRows &&
-            1 <= point.col && point.col <= numCols
-    }
-    
-    func isFoodOrSnakePoint(row: Int, col: Int, points: [Point]) -> Bool {
-        
-        for point in points {
-            if point.row == row && point.col == col {
-                return true
-            }
-        }
-        return false
-    }
-    
-    
-    mutating  func moveSnakeLeft() {
-        
-        var newSnake = [Point]()
-        
-        newSnake.append(Point(row: snake[0].row, col: snake[0].col - 1))
-        
-        for (index, _) in snake.enumerated() {
-            if index > 0 {
-                newSnake.append(snake[index - 1])
-            }
-        }
-        
-        snake = newSnake
-    }
-    
-    var description: String {
-        
-        let bottomChars = " + A B C D E F G H J K L M N O P Q R S T"
-        
-        var dots = ""
-        
-        for i in (1...numRows).reversed() {
-            if i <= 9 {
-                dots += " \(i)"
-            } else {
-                dots += "\(i)"
-            }
-            for j in 1...numCols {
-                
-                if isFoodOrSnakePoint(row: i, col: j, points: food) == true {
-                    dots += " x"
-                } else if isFoodOrSnakePoint(row: i, col: j, points: snake) == true {
-                    dots += " o"
-                } else {
-                    dots += " ."
-                }
-            }
-            dots += "\n"
-        }
-        
-        let x = numCols * 2 + 1
-        
-        let bottomCharsArray = Array(bottomChars)
-        let subString = bottomCharsArray[0...x]
-        
-        dots += subString
-        
-        return ("\(dots)")
-    }
+   
 }
