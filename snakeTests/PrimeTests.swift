f8a3404c1f9bed8b2b5fd95e875a4f9739fc61eb
Peter Shi <petershi@192.168.0.113>
2018-07-21 21:56:36 +0800

    played with numbers made out of 3's and a 1 at the end

---
+++ snakeTests/PrimeTests.swift
@@ -0,0 +1,42 @@
+//
+//  PrimeTests.swift
+//  snakeTests
+//
+//  Created by Peter Shi on 2018-07-21.
+//  Copyright © 2018 GoldThumb Inc. All rights reserved.
+//
+
+import XCTest
+
+class PrimeTests: XCTestCase {
+    
+    func testPrime() {
+        XCTAssertFalse(isPrime(candidate: 1))
+        XCTAssertTrue( isPrime(candidate: 31))
+        XCTAssertTrue( isPrime(candidate: 331))
+        XCTAssertTrue( isPrime(candidate: 3331))
+        XCTAssertTrue( isPrime(candidate: 33331))
+        XCTAssertTrue( isPrime(candidate: 333331))
+        XCTAssertTrue( isPrime(candidate: 3333331))
+        XCTAssertTrue( isPrime(candidate: 33333331))
+        XCTAssertFalse(isPrime(candidate: 333333331)) // factor: 17
+        XCTAssertFalse(isPrime(candidate: 3333333331)) // factor: 673
+        
+        XCTAssertTrue( isPrime(candidate: 673))
+        XCTAssertTrue( isPrime(candidate: 31313125397))
+        XCTAssertTrue( isPrime(candidate: 30303030303031))
+    }
+    
+    func isPrime(candidate: Int) -> Bool {
+        if candidate == 1 {
+            return false
+        }
+        for a in 2...Int(sqrt(Double(candidate))) {
+            if candidate % a == 0 {
+                print(a)
+                return false
+            }
+        }
+        return true
+    }
+}
