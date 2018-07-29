61ac950fb3fe625ed337bef386edb1262dad5847
Sharon Chen <sharonchen@Sharons-MacBook-Air.local>
2018-07-29 11:54:50 -0400

    Replaced hardcoded numbers with constants and centred grid

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -11,24 +11,33 @@ import UIKit
 class BoardView: UIView {
 
     override func draw(_ rect: CGRect) {
-        let path = UIBezierPath()
+        print(self)
+        print(frame.width)
+        print(frame.height)
         let rows = 18
         let cols = 10
         
+        let side: CGFloat = 20
+        
+        let path = UIBezierPath()
+        let originX = (frame.width - CGFloat(cols) * side) / 2
+        let originY = (frame.height - CGFloat(rows) * side) / 2
+        
+        // horizontal
+        
         for i in 0 ... rows {
-            path.move(to: CGPoint(x: 0, y: i * 30 + 30))
-            path.addLine(to: CGPoint(x: 300, y: i * 30 + 30))
+            path.move(to: CGPoint(x: originX, y: originY + CGFloat(i) * side))
+            path.addLine(to: CGPoint(x: originX + CGFloat(cols) * side, y: originY + CGFloat(i) * side))
         }
         
-        for n in 0 ... cols {
-            path.move(to: CGPoint(x: n * 30 + 30, y: 30 ))
-            path.addLine(to: CGPoint(x: n * 30 + 30, y: 600))
+        // vertical
+        
+        for i in 0 ... cols {
+            path.move(to: CGPoint(x: originX + CGFloat(i) * side, y: originY ))
+            path.addLine(to: CGPoint(x: originX + CGFloat(i) * side, y: originY + CGFloat(rows) * side))
         }
         
         path.stroke()
-        print("---------------")
     }
-
 }
 
-//homework finish grid
