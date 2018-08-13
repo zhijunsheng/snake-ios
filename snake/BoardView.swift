93cdbf55db1934928ab632094af6f2f0045be630
Peter Shi <petershi@Peters-MacBook-Pro.local>
2018-08-13 10:55:47 -0400

    added a color for the tail

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -12,13 +12,36 @@ class BoardView: UIView {
     
     let cols = 12
     let rows = 15 // hi.
-    let side: CGFloat = 20.0
+    let side: CGFloat = 23.0
+    let headColor = UIColor(red: 0/255,
+                            green: 175/255,
+                            blue: 0/255,
+                            alpha: 0.9)
     
+    let tailColor = UIColor(red: 249/255,
+                            green: 233/255,
+                            blue: 117/255,
+                            alpha: 1)
+    
+    var snakeCells = [CGPoint]()
+    var snake = [Point]()
+
     override func draw(_ rect: CGRect) {
-        // frame = (0 0; 414 736)
-        
         let originX = (frame.width -  side * CGFloat(cols)) / 2
         let originY = (frame.height - side * CGFloat(rows)) / 3
+        
+        snake = [Point(row: 15, col: 1),
+                 Point(row: 15, col: 2),
+        ]
+        
+        snakeCells = [CGPoint(x: originX, y: originY),
+                     CGPoint(x: originX + side, y: originY),
+                     //        CGPoint(x: 20.0, y: 20.0),
+            //        CGPoint(x: 20.0, y: 20.0),
+        ]
+        // calculate snakesXYs[0] and snakesXYs[1]
+//        snakesXYs[0] = //using snake[0]
+//        snakesXYs[1] = //side
         
         // horizontally draw some lines
         
@@ -42,13 +65,17 @@ class BoardView: UIView {
         
         // snake -> 🐍
         
-        for i in 0...cols - 1 {
-            drawRect(x: originX,
-                     y: originY + CGFloat(i) * side,
-                     width: side,
-                     height: side,
-                     color: .green)
-        }
+//        for i in 0...cols - 1 {
+//            drawRect(x: originX,
+//                     y: originY + CGFloat(i) * side,
+//                     width: side,
+//                     height: side,
+//                     color: .green)
+//        }
+        
+//        snakesXYs[0]
+        drawRect(x: snakeCells[0].x, y: snakeCells[0].y, width: side, height: side, color: headColor)
+        drawRect(x: snakeCells[1].x, y: snakeCells[1].y, width: side, height: side, color: .green)
     }
     
     func drawLine(fromX: CGFloat, fromY: CGFloat, toX: CGFloat, toY: CGFloat, color: UIColor) {
