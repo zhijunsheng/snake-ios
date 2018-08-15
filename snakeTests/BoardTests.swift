30315bc0bce38adbaef0a74691e1b0615ec99c20
Sharon Chen <sharonchen@Sharons-MacBook-Air.local>
2018-08-15 11:13:12 -0400

    Fixed the row number problem

--- snakeTests/BoardTests.swift
+++ snakeTests/BoardTests.swift
@@ -17,31 +17,43 @@ class BoardTests: XCTestCase {
      8 . . . . . . . . .
      7 . . . . . . . . .
      6 . . . . . . . . .
-     5 . . . . . . . . .
-     4 . . . . . . . . .
+     5 . . o o . . . . .
+     4 . o o . . . . . .
      3 . . . . . . . . .
      2 . . . . . . . . .
      1 . . . . . . . . .
      + A B C D E F G H I
     */
     func testBoard() {
-        print(boardDesc(cols: 11, rows: 11))
+        print(boardDesc(cols: 11, rows: 7))
     }
     
     func boardDesc(cols: Int, rows: Int) -> String {
+        
+        let snake = [Point(row: 8, col: 1),
+                     Point(row: 8, col: 2),
+                     Point(row: 7, col: 2),
+                     Point(row: 7, col: 3),
+                     Point(row: 7, col: 4)
+                     ]
+        
         var desc = ""
         let chars = "+ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         let charsArr = Array(chars)
-        
-        for n in 0...rows {
-            if n < 3 {
-                desc += "\(rows + 1 - n)"
+        for row in 0...rows {
+            if row < rows - 8 {
+                desc += "\(rows + 1 - row)"
             } else {
-                desc += " \(rows + 1 - n)"
+                desc += " \(rows + 1 - row)"
             }
             
-            for _ in 0...cols {
-                desc += " ."
+            for col in 0...cols {
+                if isOnSnake(row: rows + 1 - row, col: col + 1, snake: snake) {
+                    desc += " o"
+                } else {
+                    desc += " ."
+                }
+                
             }
             desc += "\n"
         }
@@ -52,5 +64,18 @@ class BoardTests: XCTestCase {
         
         return desc
     }
-}
+    
+    func isOnSnake(row: Int, col: Int, snake: [Point]) -> Bool {
+        for i in snake.indices {
+            if row == snake[i].row, col == snake[i].col {
+                return true
+            }
+        }
+        return false
+    }
 
+struct Point {
+    var row : Int
+    var col : Int
+    }
+}
