f6f059647e73cfbe816e96cda9b7501cb398e3f4
daobao <lucashongyuanzhang@gmail.com>
2019-02-16 09:03:34 +0800

    we've got a big white board

--- snake/ViewController.swift
+++ snake/ViewController.swift
@@ -1,16 +1,5 @@
-//
-//  ViewController.swift
-//  snake
-//
-//  Created by Donald Sheng on 2018-01-22.
-//  Copyright © 2018 GoldThumb Inc. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-    
 
-    
 }
-
