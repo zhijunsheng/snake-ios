1bcf5910e70e6b5571b1ce3007a3d84a5f9d2502
daobao <lucashongyuanzhang@gmail.com>
2019-03-09 10:51:37 +0800

    we have 4 roundsquears & 1 grid

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -10,20 +10,34 @@ class BoardView: UIView {
     
     override func draw(_ rect: CGRect) {
         drawGrid()
+        drawSnakeBody1(roundedRect: CGRect(x: orgX + 2 * side, y: orgY + side, width: side, height: side))
+        drawSnakeBody1(roundedRect: CGRect(x: orgX + 3 * side, y: orgY + side, width: side, height: side))
+        drawSnakeBody1(roundedRect: CGRect(x: orgX + 3 * side, y: orgY + 2 * side, width: side, height: side))
+        drawSnakeBody1(roundedRect: CGRect(x: orgX + 4 * side, y: orgY + 2 * side, width: side, height: side))
+    }
+    
+    func drawSnakeBody1(roundedRect: CGRect) {
+        let sB1 = UIBezierPath(roundedRect: roundedRect, cornerRadius: 5)
+        #colorLiteral(red: 0, green: 0, blue: 1, alpha: 1).setFill()
+        sB1.fill()
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
+        sB1.stroke()
+        
     }
     
     func drawGrid() {
-        let h8 = UIBezierPath()
+        let path = UIBezierPath()
         for i in 0 ... cols {
-            h8.move(to: CGPoint(x: orgX + CGFloat(i) * side, y: orgY))
-            h8.addLine(to: CGPoint(x: orgX + CGFloat(i) * side, y:orgY + CGFloat(rows) * side))
+            path.move(to: CGPoint(x: orgX + CGFloat(i) * side, y: orgY))
+            path.addLine(to: CGPoint(x: orgX + CGFloat(i) * side, y:orgY + CGFloat(rows) * side))
         }
         
         for i in 0 ... rows {
-            h8.move(to: CGPoint(x: orgX, y: orgY + CGFloat(i) * side))
-            h8.addLine(to: CGPoint(x: orgX + CGFloat(cols) * side, y: orgY + CGFloat(i) * side))
+            path.move(to: CGPoint(x: orgX, y: orgY + CGFloat(i) * side))
+            path.addLine(to: CGPoint(x: orgX + CGFloat(cols) * side, y: orgY + CGFloat(i) * side))
         }
+        
         #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1).setStroke()
-        h8.stroke()
+        path.stroke()
     }
 }
