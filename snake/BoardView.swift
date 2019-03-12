c46c785d0bf37416c97c29d1c19921f1e4f4acc1
daobao <lucashongyuanzhang@gmail.com>
2019-03-12 20:28:59 +0800

    we have a snake array and a rainbowSnake in it

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -8,19 +8,32 @@ class BoardView: UIView {
     let orgX: CGFloat = 20
     let orgY: CGFloat = 50
     
+    var snake: [SnakeCell] = [
+        SnakeCell(col: 2, row: 1, color: #colorLiteral(red: 1, green: 0.3098039216, blue: 0.2823529412, alpha: 1)),
+        SnakeCell(col: 3, row: 1, color: #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)),
+        SnakeCell(col: 3, row: 2, color: #colorLiteral(red: 0.9843137255, green: 1, blue: 0, alpha: 1)),
+        SnakeCell(col: 4, row: 2, color: #colorLiteral(red: 0, green: 0.831372549, blue: 0.1725490196, alpha: 1)),
+        SnakeCell(col: 4, row: 3, color: #colorLiteral(red: 0, green: 1, blue: 1, alpha: 1)),
+        SnakeCell(col: 4, row: 4, color: #colorLiteral(red: 0, green: 0.4156862745, blue: 1, alpha: 1)),
+        SnakeCell(col: 3, row: 4, color: #colorLiteral(red: 0.5215686275, green: 0.3764705882, blue: 0.7490196078, alpha: 1))
+    ]
+    
     override func draw(_ rect: CGRect) {
         drawGrid()
-        drawSnakeBody1(roundedRect: CGRect(x: orgX + 2 * side, y: orgY + side, width: side, height: side))
-        drawSnakeBody1(roundedRect: CGRect(x: orgX + 3 * side, y: orgY + side, width: side, height: side))
-        drawSnakeBody1(roundedRect: CGRect(x: orgX + 3 * side, y: orgY + 2 * side, width: side, height: side))
-        drawSnakeBody1(roundedRect: CGRect(x: orgX + 4 * side, y: orgY + 2 * side, width: side, height: side))
+        drawSnakeBodys()
     }
     
-    func drawSnakeBody1(roundedRect: CGRect) {
+    func drawSnakeBodys() {
+        for cell in snake {
+            drawSnakeBody1(roundedRect: CGRect(x: orgX + CGFloat(cell.col) * side, y: orgY + CGFloat(cell.row) * side, width: side, height: side), color:  cell.color)
+        }
+    }
+    
+    func drawSnakeBody1(roundedRect: CGRect, color: UIColor) {
         let sB1 = UIBezierPath(roundedRect: roundedRect, cornerRadius: 5)
-        #colorLiteral(red: 0, green: 0, blue: 1, alpha: 1).setFill()
+        color.setFill()
         sB1.fill()
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
+        #colorLiteral(red: 0, green: 0.7529411765, blue: 0.9921568627, alpha: 1).setStroke()
         sB1.stroke()
         
     }
---
+++ snake/SnakeCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+struct SnakeCell {
+    let col: Int
+    let row: Int
+    let color: UIColor
+}
