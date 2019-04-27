367215a1b49c5216e0376e87c83094cdf7e9156b
ethan <ethanyiyaozhang@gmail.com>
2019-04-27 19:58:05 +0800

    we fixed bugs

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -1,8 +1,29 @@
 import UIKit
 
 class BoardView: UIView {
+    
+    let cols: Int = 8
+    let rows: Int = 28
+    let originX: CGFloat = 10
+    let originY: CGFloat = 10
+    let cellSide: CGFloat = 10
+    
     override func draw(_ rect: CGRect) {
        
+        let pen = UIBezierPath()
+        
+        for i in 0..<rows + 1 {
+            pen.move(to: CGPoint(x: originX, y: cellSide * CGFloat(i) + originY))
+            pen.addLine(to: CGPoint(x: originX + CGFloat(cols) * cellSide, y: cellSide * CGFloat(i) + originY))
+        }
+        
+        for i in 0..<cols + 1 {
+            pen.move(to: CGPoint(x: cellSide * CGFloat(i) + originX, y: originY))
+            pen.addLine(to: CGPoint(x: cellSide * CGFloat(i) + originX, y: originY + CGFloat(rows) * cellSide))
+        }
+        
+        pen.stroke()
+        
     }
 
 }
