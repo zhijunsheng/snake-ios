f4a26dd6bdc0e90bdd8634ee6f3904f15e8e01e2
daobao <lucashongyuanzhang@gmail.com>
2019-04-27 11:03:19 +0800

    we knew that 200~300 years ago,mathematicians's think is wrong❌

---
+++ snakeTests/PrimeNumbersTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+
+class PrimeNumbersTests: XCTestCase {
+    
+    
+    func testPrimeNumbers() {
+        let num = 333333331 // 331, 3331, 33331, ...
+        for prnu in 2..<num {
+            let remainder = num % prnu
+            if remainder == 0 {
+                print("⛔️")
+                let answer = num / prnu
+                print(answer)
+            }
+        }
+    }
+}
