0a1e9c1cee7d20d114484e8295786c8e690f3782
ethan <ethanyiyaozhang@gmail.com>
2019-05-01 19:48:07 +0800

    we drew the grid

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -2,26 +2,26 @@ import UIKit
 
 class BoardView: UIView {
     
-    let cols: Int = 8
-    let rows: Int = 28
+    let cols: Int = 10
+    let rows: Int = 19
     let originX: CGFloat = 10
     let originY: CGFloat = 10
-    let cellSide: CGFloat = 10
+    let cellSide: CGFloat = 27
     
     override func draw(_ rect: CGRect) {
        
         let pen = UIBezierPath()
         
-        for i in 0..<rows + 1 {
-            pen.move(to: CGPoint(x: originX, y: cellSide * CGFloat(i) + originY))
-            pen.addLine(to: CGPoint(x: originX + CGFloat(cols) * cellSide, y: cellSide * CGFloat(i) + originY))
+        for i in 0...rows {
+            pen.move(to: CGPoint(x: originX, y: originY + CGFloat(i) * cellSide))
+            pen.addLine(to: CGPoint(x: originX + CGFloat(cols) * cellSide, y: originY + CGFloat(i) * cellSide))
         }
         
-        for i in 0..<cols + 1 {
-            pen.move(to: CGPoint(x: cellSide * CGFloat(i) + originX, y: originY))
-            pen.addLine(to: CGPoint(x: cellSide * CGFloat(i) + originX, y: originY + CGFloat(rows) * cellSide))
+        for i in 0...cols {
+            pen.move(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY ))
+            pen.addLine(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + CGFloat(rows) * cellSide))
         }
-        
+
         pen.stroke()
         
     }
