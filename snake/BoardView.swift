fd9ba632d0d9915c16003f6b491b85340193d783
ethan <ethanyiyaozhang@gmail.com>
2019-05-04 19:30:39 +0800

    we let snake move.

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -3,13 +3,25 @@ import UIKit
 class BoardView: UIView {
     
     let cols: Int = 10
-    let rows: Int = 19
+    let rows: Int = 15
     let originX: CGFloat = 10
     let originY: CGFloat = 10
     let cellSide: CGFloat = 27
     
+    //snake
+    var x: Int = 7
+    var y: Int = 4
+    
     override func draw(_ rect: CGRect) {
-       
+        drawGrid()
+        
+        let pencil = UIBezierPath(roundedRect: CGRect(x: originX + CGFloat(x) * cellSide, y: originY + CGFloat(y) * cellSide, width: cellSide, height: cellSide), cornerRadius: 4)
+        
+        #colorLiteral(red: 0, green: 0.8426381946, blue: 1, alpha: 1).setFill()
+        pencil.fill()
+    }
+    
+    func drawGrid() {
         let pen = UIBezierPath()
         
         for i in 0...rows {
@@ -21,9 +33,8 @@ class BoardView: UIView {
             pen.move(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY ))
             pen.addLine(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + CGFloat(rows) * cellSide))
         }
-
-        pen.stroke()
         
+        pen.stroke()
     }
-
+    
 }
--- snake/GameViewController.swift
+++ snake/GameViewController.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var boardView: BoardView!
+    
+    @IBAction func touchLeft(_ sender: UIButton) {
+        boardView.x = boardView.x - 1
+        boardView.setNeedsDisplay()
+    }
 }
