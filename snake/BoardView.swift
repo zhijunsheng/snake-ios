5c0b28482308c867ecf845aee8761f105307a5e0
ethan <ethanyiyaozhang@gmail.com>
2019-05-08 19:57:18 +0800

    we made 2 cells move

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -9,16 +9,32 @@ class BoardView: UIView {
     let cellSide: CGFloat = 27
     
     //snake
-    var x: Int = 7
-    var y: Int = 4
+    var x0: Int = 5
+    var y0: Int = 5
+    
+    var x1: Int = 5
+    var y1: Int = 6
+    
+    var x2: Int = 5
+    var y2: Int = 7
     
     override func draw(_ rect: CGRect) {
         drawGrid()
         
-        let pencil = UIBezierPath(roundedRect: CGRect(x: originX + CGFloat(x) * cellSide, y: originY + CGFloat(y) * cellSide, width: cellSide, height: cellSide), cornerRadius: 4)
+        let pencil0 = UIBezierPath(roundedRect: CGRect(x: originX + CGFloat(x0) * cellSide, y: originY + CGFloat(y0) * cellSide, width: cellSide, height: cellSide), cornerRadius: 4)
+        
+        let pencil1 = UIBezierPath(roundedRect: CGRect(x: originX + CGFloat(x1) * cellSide, y: originY + CGFloat(y1) * cellSide, width: cellSide, height: cellSide), cornerRadius: 4)
+        
+        let pencil2 = UIBezierPath(roundedRect: CGRect(x: originX + CGFloat(x2) * cellSide, y: originY + CGFloat(y2) * cellSide, width: cellSide, height: cellSide), cornerRadius: 4)
         
         #colorLiteral(red: 0, green: 0.8426381946, blue: 1, alpha: 1).setFill()
-        pencil.fill()
+        pencil0.fill()
+        
+        #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1).setFill()
+        pencil1.fill()
+        
+        #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1).setFill()
+        pencil2.fill()
     }
     
     func drawGrid() {
--- snake/GameViewController.swift
+++ snake/GameViewController.swift
@@ -5,15 +5,35 @@ class GameViewController: UIViewController {
     @IBOutlet weak var boardView: BoardView!
     
     @IBAction func touchLeft(_ sender: UIButton) {
-        boardView.x = boardView.x - 1
+        // x0 = 3, y0 = 0
+        // x1 = 3, y1 = 0
+        boardView.x1 = boardView.x0
+        boardView.y1 = boardView.y0
+
+
+
+        boardView.x0 -= 1
         boardView.setNeedsDisplay()
     }
     
     @IBAction func touchUp(_ sender: UIButton) {
-        boardView.y = boardView.y - 1
+        boardView.x1 = boardView.x0
+        boardView.y1 = boardView.y0
+        boardView.y0 -= 1
         boardView.setNeedsDisplay()
     }
     
-    // Down
-    // Right ->
+    @IBAction func touchDown(_ sender: UIButton) {
+        boardView.x1 = boardView.x0
+        boardView.y1 = boardView.y0
+        boardView.y0 += 1
+        boardView.setNeedsDisplay()
+    }
+    
+    @IBAction func touchRight(_ sender: UIButton) {
+        boardView.x1 = boardView.x0
+        boardView.y1 = boardView.y0
+        boardView.x0 += 1
+        boardView.setNeedsDisplay()
+    }
 }
