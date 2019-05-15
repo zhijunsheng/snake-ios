8ef359d74d4187ba09dbe75b07e86b9aefd699d2
ethan <ethanyiyaozhang@gmail.com>
2019-05-15 19:02:23 +0800

    We do not allow snake to go out.

--- snake/GameViewController.swift
+++ snake/GameViewController.swift
@@ -5,38 +5,46 @@ class GameViewController: UIViewController {
     @IBOutlet weak var boardView: BoardView!
     
     @IBAction func touchLeft(_ sender: UIButton) {
-        boardView.x2 = boardView.x1
-        boardView.y2 = boardView.y1
-        boardView.x1 = boardView.x0
-        boardView.y1 = boardView.y0
-        boardView.x0 -= 1
-        boardView.setNeedsDisplay()
+        if boardView.x0 > 0 {
+            boardView.x2 = boardView.x1
+            boardView.y2 = boardView.y1
+            boardView.x1 = boardView.x0
+            boardView.y1 = boardView.y0
+            boardView.x0 -= 1
+            boardView.setNeedsDisplay()
+        }
     }
     
     @IBAction func touchUp(_ sender: UIButton) {
-        boardView.x2 = boardView.x1
-        boardView.y2 = boardView.y1
-        boardView.x1 = boardView.x0
-        boardView.y1 = boardView.y0
-        boardView.y0 -= 1
-        boardView.setNeedsDisplay()
+        if boardView.y0 > 0 {
+            boardView.x2 = boardView.x1
+            boardView.y2 = boardView.y1
+            boardView.x1 = boardView.x0
+            boardView.y1 = boardView.y0
+            boardView.y0 -= 1
+            boardView.setNeedsDisplay()
+        }
     }
     
     @IBAction func touchDown(_ sender: UIButton) {
-        boardView.x2 = boardView.x1
-        boardView.y2 = boardView.y1
-        boardView.x1 = boardView.x0
-        boardView.y1 = boardView.y0
-        boardView.y0 += 1
-        boardView.setNeedsDisplay()
+        if boardView.y0 < 14 {
+            boardView.x2 = boardView.x1
+            boardView.y2 = boardView.y1
+            boardView.x1 = boardView.x0
+            boardView.y1 = boardView.y0
+            boardView.y0 += 1
+            boardView.setNeedsDisplay()
+        }
     }
     
     @IBAction func touchRight(_ sender: UIButton) {
-        boardView.x2 = boardView.x1
-        boardView.y2 = boardView.y1
-        boardView.x1 = boardView.x0
-        boardView.y1 = boardView.y0
-        boardView.x0 += 1
-        boardView.setNeedsDisplay()
+        if boardView.x0 < 9 {
+            boardView.x2 = boardView.x1
+            boardView.y2 = boardView.y1
+            boardView.x1 = boardView.x0
+            boardView.y1 = boardView.y0
+            boardView.x0 += 1
+            boardView.setNeedsDisplay()
+        }
     }
 }
