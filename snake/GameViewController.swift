17b8d057a082341a03569bd03ae8a2d338f5596c
Andy Yan <andy@Andys-MacBook-Air.local>
2019-11-19 20:23:36 -0500

    we cleaned up the project

--- snake/GameViewController.swift
+++ snake/GameViewController.swift
@@ -10,96 +10,13 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    let numRows = 21
-    let numCols = 12
-    
-    @IBOutlet var boardView: BoardView!
-    var board = Board(numRows: 0, numCols: 0, snake: [], food: [])
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let snake: [Point] = [
-            Point(row: 4, col: 4),
-            Point(row: 4, col: 5),
-            Point(row: 3, col: 5),
-            Point(row: 3, col: 6),
-            Point(row: 3, col: 7),
-        ]
-        
-        let food = [
-            Point(row: 10, col: 4),
-            Point(row: 8, col: 10),
-        ]
-        board = Board(numRows: numRows, numCols: numCols, snake: snake, food: food)
-        updateViewFromModel()
+     
     }
     
-    @IBAction func leftButton(_ sender: Any) {
-//        moveSnakeToLeft()
-        board.moveSnakeLeft()
-        
-        updateViewFromModel()
-    }
-    
-    
-    @IBAction func buttonTapped(_ sender: Any) {
-        // modify our model, e.g change the food, or snake
-//        let newSnake = [
-//            Point(row: 2, col: 2),
-//            Point(row: 2, col: 3),
-//            Point(row: 2, col: 4)
-//        ]
-//
-//        board = Board(numRows: numRows, numCols: numCols, snake: newSnake, food: board.food)
-//
-//        print(board)
-
-        updateViewFromModel()
-    }
-    
-    private func location(of point: Point, inBoardView boardView: BoardView) -> CGPoint {
-        let centerX = boardView.gridTopLeft.x + CGFloat(point.col - 1) * boardView.cellSide
-        let centerY = boardView.gridTopLeft.y + CGFloat(boardView.gridHeight - point.row) * boardView.cellSide
-        return CGPoint(x: centerX, y: centerY)
-    }
-    
-    func updateViewFromModel() {
-        boardView.gridHeight = board.numRows
-        boardView.gridWidth = board.numCols
-        boardView.cellSide = 20
-        boardView.gridTopLeft = CGPoint(x: 20.0, y: 20.0)
-        
-        // based on our model, i.e. board
-        // we can draw on boardView with all the info in board
-        
-        var newFoodOnBoardView = [CGPoint]()
-        
-        for i in board.food {
-            newFoodOnBoardView.append(convertFrom(point: i))
-        }
-        
-        boardView.food = newFoodOnBoardView
-        
-        var newSnakeOnBoardView = [CGPoint]()
-        
-        for num in board.snake {
-            print(num)
-        
-            newSnakeOnBoardView.append(convertFrom(point: num))
-        }
-        
-        boardView.snake = newSnakeOnBoardView
-        
-        boardView.setNeedsDisplay()
-    }
-    
-    func convertFrom(point: Point) -> CGPoint {
-        let x = boardView.gridTopLeft.x + boardView.cellSide * CGFloat(point.col)
-        let y = boardView.gridTopLeft.y + boardView.cellSide * CGFloat(boardView.gridHeight - point.row)
-        
-        return CGPoint(x: x, y: y)
-    }
     
 }
 
