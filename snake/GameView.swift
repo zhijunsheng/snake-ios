7defaf92fc7fdfaa3f8c891bb7c02da0ab23c462
Angie Yan <angieyan@Andys-MacBook-Air.local>
2019-11-20 16:42:54 -0500

    I started on the grid for the snake game.

--- snake/GameView.swift
+++ snake/GameView.swift
@@ -9,19 +9,26 @@
 import UIKit
 
 class GameView: UIView {
-
-    
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         
-        let bPath = UIBezierPath()
-        bPath.move(to: CGPoint(x: 0, y: 0))
-        bPath.addLine(to: CGPoint(x: 100, y: 100))
-        #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1).setStroke()
-        bPath.stroke()
-        // Drawing code
+        // Horizontal Lines
+        
+        for i in 0..<15 {
+            let pencil = UIBezierPath()
+            pencil.move(to: CGPoint(x: 0, y: 40 * CGFloat(i)))
+            pencil.addLine(to: CGPoint(x: 600, y: 40 * CGFloat(i)))
+            #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setStroke()
+            pencil.stroke()
+        }
+        
+        // Vertical lines
+        
+        for i in 0..<15 {
+            let pencil = UIBezierPath()
+            pencil.move(to: CGPoint(x: 40 * CGFloat(i), y: 600))
+            pencil.addLine(to: CGPoint(x: 40 * CGFloat(i), y: 0))
+            #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0).setStroke()
+            pencil.stroke()
+        }
     }
- 
-
 }
