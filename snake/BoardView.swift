afe45008147df7e093c16be3a22e017002744d2d
Andy Yan <andy@Andys-MacBook-Air.local>
2020-04-28 17:15:30 -0400

    we defined 2 more constants to move the grid

--- snake/BoardView.swift
+++ snake/BoardView.swift
@@ -10,20 +10,20 @@ import UIKit
 
 class BoardView: UIView {
     
-    var cellSide = 50
+    let cellSide: CGFloat = 50
+    let originX: CGFloat = 187
+    let originY: CGFloat = 100
     
-    var y = 500
-    var x = 80
+    
+    var y: CGFloat = 500
+    var x: CGFloat = 80
+
     
     
     override func draw(_ rect: CGRect) {
         
         drawGrid()
-        
-        let pencil2 = UIBezierPath(arcCenter: CGPoint(x: x + cellSide / 2, y: y - cellSide / 2), radius: CGFloat(cellSide / 2), startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
-        
-        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
-        pencil2.stroke()
+        drawSnake()
         
     }
     
@@ -31,19 +31,34 @@ class BoardView: UIView {
         let pencil = UIBezierPath()
 
         for i in 0 ... 10 {
-            pencil.move(to: CGPoint(x: 80, y: 100 + i * cellSide))
-            pencil.addLine(to: CGPoint(x: 80 + cellSide * 10, y: 100 + i * cellSide))
+            pencil.move(to: CGPoint(x: originX, y: originY + CGFloat(i) * cellSide))
+            pencil.addLine(to: CGPoint(x: originX + cellSide * 10, y: originY + CGFloat(i) * cellSide))
             
         }
         
         for i in 0 ... 10 {
-            pencil.move(to: CGPoint(x: 80 + i * cellSide, y: 100))
-            pencil.addLine(to: CGPoint(x: 80 + i * cellSide, y: 100 + cellSide * 10))
+            pencil.move(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY))
+            pencil.addLine(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + cellSide * 10))
             
         }
         
         #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1).setStroke()
         pencil.stroke()
         
+    }
+    
+    func drawSnake() {
+        drawSnakeCell(col: 0, row: 0)
+        
+    }
+    
+    func drawSnakeCell(col: Int, row: Int) {
+        let centerX: CGFloat = 80 + cellSide / 2
+        let centerY: CGFloat = 100 + cellSide / 2
+        
+        let pencil2 = UIBezierPath(arcCenter: CGPoint(x: centerX, y: centerY), radius: CGFloat(cellSide / 2), startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+        
+        #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1).setStroke()
+        pencil2.stroke()
     }
 }
