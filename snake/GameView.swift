2c99e498644e4b883c21c97a141adb0d47a516e6
Angie Yan <angieyan@Angies-MacBook-Pro.local>
2020-05-02 16:31:33 -0400

    I made the head of the snake a different colour.

--- snake/GameView.swift
+++ snake/GameView.swift
@@ -29,11 +29,11 @@ class GameView: UIView {
         snake.append(SnakeCell(col: 5, row: 2))
         snake.append(SnakeCell(col: 6, row: 2))
         snake.append(SnakeCell(col: 7, row: 2))
-        snake.append(SnakeCell(col: 8, row: 2))
         
         for i in 0..<snake.count {
-            drawSnakeCell(col: snake[i].col, row: snake[i].row, colour: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
+            drawSnakeCell(col: snake[i].col, row: snake[i].row, colour: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
         }
+        drawSnakeCell(col: 8, row: 2, colour: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
     }
     
     func drawSnakeCell(col: Int, row: Int, colour: UIColor) {
