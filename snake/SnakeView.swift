d07cbb47e29793d07929127b1d9af82f47dba102
Lambert Lin <lambertsixianlin@gmail.com>
2021-12-17 21:31:14 +0800

    We made the game board flexible.

--- snake/SnakeView.swift
+++ snake/SnakeView.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class SnakeView: UIView {
-
- 
+    let boardSize: Int = 9
+    
+    let cellSide: CGFloat = 49.2
+    let originX: CGFloat = 100
+    let originY: CGFloat = 100
+    
+    
     override func draw(_ rect: CGRect) {
-        let pencil = UIBezierPath()
-        pencil.move(to: CGPoint(x: 12, y: 12))
-        pencil.addLine(to: CGPoint(x: 12, y: 538))
-        pencil.stroke()
-        
-        
+        drawBoard()
     }
-
+    
+    func drawBoard() {
+        let pencil = UIBezierPath()
+        for i in 0..<boardSize + 1 {
+            pencil.move(to: CGPoint(x: originX + CGFloat(i) * cellSide, y: originX))
+            pencil.addLine(to: CGPoint(x:  originX + CGFloat(i) * cellSide, y: originX + CGFloat(boardSize) * cellSide))
+            pencil.move(to: CGPoint(x:  originX, y: originY + CGFloat(i) * cellSide ))
+            pencil.addLine(to: CGPoint(x:  originX + CGFloat(boardSize) * cellSide , y: originY + CGFloat(i) * cellSide ))
+        }
+        pencil.stroke()
+    }
 }
